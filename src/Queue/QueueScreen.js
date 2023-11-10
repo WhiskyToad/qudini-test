83b6dcf21b2b3a331de6dcd1d324960bb2ea08b2
Steven Craig <stevencraig321@gmail.com>
2023-11-10 11:02:44 +0000

    feat: refetches every 30s

--- src/Queue/QueueScreen.js
+++ src/Queue/QueueScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { fetchQueueData } from "../mockApi";
 import CustomerDetails from "./components/Customer";
 import TextInput from "./components/TextInput";
@@ -7,13 +7,28 @@ const QueueScreen = () => {
   const [customers, setCustomers] = useState([]);
   const [inputValue, setInputValue] = useState("");
 
-  useEffect(() => {
-    fetchQueueData()
-      .then((response) => response.json())
-      .then((json) => {
-        setCustomers(json.queueData.queue.customersToday);
-      });
+  const fetchQueueDataCB = useCallback(async () => {
+    try {
+      const response = await fetchQueueData(); // Assuming you have a function for fetching data
+      const json = await response.json();
+      setCustomers(json.queueData.queue.customersToday);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
   }, []);
+
+  useEffect(() => {
+    //initial fetch
+    fetchQueueDataCB();
+
+    // Fetch data every 30 seconds
+    const intervalId = setInterval(() => {
+      fetchQueueDataCB();
+    }, 30000);
+
+    // Clean up the interval when the component unmounts
+    return () => clearInterval(intervalId);
+  }, [fetchQueueDataCB]);
 
   return (
     <>
